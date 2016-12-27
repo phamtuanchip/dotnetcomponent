725a1afe486b4ab29bfd70d787178cf5c7fbe24c
Tuan Pham <phamtuanchip@gmail.com>
2016-12-28 03:13:30 +0700

    update rest test

---
+++ MVC03/MVC03/Controllers/RestController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using System.Web.Http;
+
+using System.Web;
+using System.Web.Mvc;
+
+
+using System.IO;
+using System.Web.Http.Results;
+using MVC03.Filter;
+
+namespace MVC03.Controllers
+{
+   
+    public class RestController : ApiController
+    {
+        [System.Web.Http.HttpGet]
+        [AllowJsonGet]
+        public JsonResult Test() {
+            return new JsonResult();
+        }
+
+        [System.Web.Http.HttpGet]
+        [AllowJsonGet]
+        public String Teststring()
+        {
+            return "test";
+        }
+
+        [System.Web.Http.HttpPost]
+        public JsonResult Upload(HttpPostedFile file)
+        {
+            return new JsonResult();
+        }
+
+        [System.Web.Http.HttpGet]
+        [AllowJsonGet]
+        public JsonResult<DirectoryInfo> LoadFolder(String folderName)
+        {
+            DirectoryInfo di = new DirectoryInfo("C:\\"+ folderName);
+            String[] folders = System.IO.Directory.GetDirectories(@"C:\"+ folderName, "*", System.IO.SearchOption.AllDirectories);
+
+            foreach (string f in folders)
+            {
+                //call some function to get all files in folder
+            }
+            return Json(di);
+        }
+
+        [System.Web.Http.HttpGet]
+        [AllowJsonGet]
+        public DirectoryInfo LoadRoot(String folderName)
+        {
+            DirectoryInfo di = new DirectoryInfo("C:\\" + folderName);
+
+            return di;
+        }
+    }
+}
---
+++ MVC03/MVC03/Filter/AllowJsonGetAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Web.Mvc;
+
+namespace MVC03.Filter
+{
+    public sealed class AllowJsonGetAttribute : System.Web.Mvc.ActionFilterAttribute, System.Web.Mvc.IActionFilter
+    {
+        void System.Web.Mvc.IActionFilter.OnActionExecuted(ActionExecutedContext context)
+        {
+            var jsonResult = context.Result as JsonResult;
+            if (jsonResult == null) return;
+
+            jsonResult.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            var jsonResult = filterContext.Result as JsonResult;
+            if (jsonResult == null) return;
+
+            jsonResult.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            base.OnResultExecuting(filterContext);
+        }
+    }
+}
